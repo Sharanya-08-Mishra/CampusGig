60d27d110b14b7cbdd1c762b7bd8b5c7caa271f6
Vanshika Rao <raovanshika78@gmail.com>
2026-09-23 20:42:26 +0530

    Add initial Student servlet

---
+++ src/main/java/com/campusgig/controller/StudentServlet.java
@@ -0,0 +1,21 @@
+package com.campusgig.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/student")
+public class StudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>CampusGig Student Servlet is working!</h1>");
+    }
+}
